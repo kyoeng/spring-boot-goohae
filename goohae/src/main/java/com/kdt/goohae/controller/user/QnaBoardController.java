6d90af0da7ef055ab8ef04f4cafeb58d9fe96cc9
yujeonghyeon <dbdb1114@naver.com>
2023-03-10 15:52:10 +0900

    mod수정

--- goohae/src/main/java/com/kdt/goohae/controller/user/QnaBoardController.java
+++ goohae/src/main/java/com/kdt/goohae/controller/user/QnaBoardController.java
@@ -5,7 +5,6 @@ import com.kdt.goohae.domain.forPaging.PageMaker;
 import com.kdt.goohae.domain.forPaging.SearchCri;
 import com.kdt.goohae.domain.user.QnaBoardVO;
 import com.kdt.goohae.service.user.QnaBoardService;
-import com.sun.org.apache.xpath.internal.operations.Mod;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
