e1547476cdfeebf4a38333cd2f20f6e7556d61f5
DESKTOP-IQ624AA\yk_Ha <proguy425@gmail.com>
2023-03-13 18:13:29 +0900

    qna table 작성, css수정,

--- goohae/src/main/resources/static/js/customer/inquiry.js
+++ goohae/src/main/resources/static/js/customer/inquiry.js
@@ -1,2 +1,18 @@
-const formData = new FormData();
+const form = document.querySelector('#etcForm');
 
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const payLoad = new FormData(form);
+
+    fetch('/logined-user/qna-board/insert', {
+        method: 'Post',
+        header: {
+            'Content-Type': 'json',
+            'sameSite': "None",
+            'Secure': 'true'
+        },
+        body: payLoad
+    })
+        .catch(err => console.log(err))
+})
