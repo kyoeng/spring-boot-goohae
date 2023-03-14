098dd559bdf6f909b69935e1fa527fb5cdee98e5
yujeonghyeon <dbdb1114@naver.com>
2023-03-14 17:28:41 +0900

    wishlistController 삭제 추가

--- goohae/src/main/java/com/kdt/goohae/controller/user/WishController.java
+++ goohae/src/main/java/com/kdt/goohae/controller/user/WishController.java
@@ -1,10 +1,12 @@
 package com.kdt.goohae.controller.user;
 
+import com.kdt.goohae.domain.user.WishVO;
 import com.kdt.goohae.service.user.WishService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpSession;
 
@@ -27,5 +29,27 @@ public class WishController {
      * */
     @PostMapping(value = "logined-user/mywish")
     public Model myWish(Model model, HttpSession httpSession) {return model.addAttribute("wishList",wishService.selectList((String) httpSession.getAttribute("loginId")));}
+
+    /**
+     * wishList에서 상품 삭제
+     * param : productCode
+     * */
+    @GetMapping(value = "logined-user/mywish/delete")
+    public String delete (WishVO vo, HttpSession httpSession){
+        vo.setUserId((String) httpSession.getAttribute("loginId"));
+        wishService.delete(vo);
+        return "/user/myPage/wishList";
+    }
+
+    /**
+     * 체크된 항목 삭제
+     * param : productCode의 배열 ( productCodes로 해주세요. )
+     * */
+    @GetMapping(value = "logined-user/mywish/checked-delete")
+    public String checkedDelete (WishVO vo, HttpSession httpSession){
+        vo.setUserId((String) httpSession.getAttribute("loginId"));
+        wishService.checkedDelete(vo);
+        return "/user/myPage/wishList";
+    }
 
 }
