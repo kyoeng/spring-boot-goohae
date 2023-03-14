9107099abe4860f469fd2413272ebd33c2f96cd8
DESKTOP-IQ624AA\yk_Ha <proguy425@gmail.com>
2023-03-14 21:25:23 +0900

    detail 중간커밋 삭제기능 구현중

--- goohae/src/main/resources/static/js/customer/inquiry.js
+++ goohae/src/main/resources/static/js/customer/inquiry.js
@@ -5,30 +5,51 @@ $(document).ready(function () {
     btn.click(function () {
         const formData = new FormData();
 
-        if ($('input[name="title"]').val() !== "" &&
-            $('textarea[name="content"]').val() !== "" &&
-            $('input[name="boardPassword"]').val() !== "") {
-            formData.append('title', $('input[name="title"]').val());
-            formData.append('content', $('textarea[name="content"]').val());
-            formData.append('boardPassword', $('input[name="boardPassword"]').val());
+        formData.append('title', $('input[name="title"]').val());
+        formData.append('content', $('textarea[name="content"]').val());
 
-            $.ajax({
-                url: '/logined-user/qna-board/insert',
-                type: 'POST',
-                processData: false,
-                contentType: false,
-                data: formData
-            })
-                .done((data, status) => {
-                    location.href = '/qna-board/list'
-                })
-                .fail((err, status) => {
-                    console.log(err)
-                })
+        if ($('input[name="boardPassword"]').val() == '') {
+            formData.append('boardPassword', null);
         } else {
-            return alert('정보가 입력되지 않았습니다.');
+            formData.append('boardPassword', 'input[name="boardPassword"]').val()
         }
+        $.ajax({
+            url: '/logined-user/qna-board/insert',
+            type: 'POST',
+            processData: false,
+            contentType: false,
+            data: formData
+        })
+            .done((data, status) => {
+                console.log(data);
+                location.href = '/qna-board/list';
+            })
+            .fail((err, status) => {
+                console.log(err)
+            })
+        // if ($('input[name="title"]').val() !== "" &&
+        //     $('textarea[name="content"]').val() !== "" &&
+        //     $('input[name="boardPassword"]').val() == "") {
+        //     formData.append('title', $('input[name="title"]').val());
+        //     formData.append('content', $('textarea[name="content"]').val());
+        //     formData.append('boardPassword', $('input[name="boardPassword"]').val());
+
+        //     $.ajax({
+        //         url: '/logined-user/qna-board/insert',
+        //         type: 'POST',
+        //         processData: false,
+        //         contentType: false,
+        //         data: formData
+        //     })
+        //         .done((data, status) => {
+        //             location.href = '/qna-board/list';
+        //         })
+        //         .fail((err, status) => {
+        //             console.log(err)
+        //         })
+        // } else {
+        //     return alert('정보가 입력되지 않았습니다.');
+        // }
     })
 })
-
 
--- goohae/src/main/resources/static/js/customer/qnaList.js
+++ goohae/src/main/resources/static/js/customer/qnaList.js
@@ -1,5 +1,4 @@
 $('tr[id^="qnaRow"]').click(function () {
     let boardSeq = $(this).attr('id').replace('qnaRow', '');
-
-    location.href = `/qna-board/detail/boardSeq=${boardSeq}`
+    location.href = `/qna-board/detail?boardSeq=${boardSeq}&boardPassword=${null}`
 })
